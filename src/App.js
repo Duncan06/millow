4a3ddaf7e899861ee6405afc3f9f8b2c8ec42f6b
duncan06 <tylerdownie0@gmail.com>
2024-01-05 15:48:22 -0800

    Add loading state and logic to approve purchases with all parties involved.

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ import Escrow from './abis/Escrow.json'
 import config from './config.json';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
   const [provider, setProvider] = useState(null)
   const [escrow, setEscrow] = useState(null)
 
   const [account, setAccount] = useState(null)
   const [homes, setHomes] = useState(null)
+  const [home, setHome] = useState(null)
+  const [toggle, setToggle] = useState(false)
 
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
@@ -47,43 +50,60 @@ function App() {
       const account = ethers.utils.getAddress(accounts[0])
       setAccount(account)
     })
+
+    setIsLoading(false)
   }
 
   useEffect(() => {
     loadBlockchainData()
   }, [])
 
+  const togglePop = (home) => {
+    setHome(home)
+    toggle ? setToggle(false) : setToggle(true)
+  }
+
   return (
     <div>
-      <Navigation account={account} setAccount={setAccount} />
-      <Search />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+      <div>
+        <Navigation account={account} setAccount={setAccount} />
+        <Search />
 
-      <div className='cards__section'>
+        <div className='cards__section'>
 
-        <h3>Homes For You</h3>
+          <h3>Homes For You</h3>
 
-        <hr />
+          <hr />
 
-        <div className='cards'>
-          {homes.map((home, index) => (
-            <div className='card' key={index}>
-            <div className='card__image'>
-              <img src={home.image} alt="Home"/>
+          <div className='cards'>
+            {homes.map((home, index) => (
+              <div className='card' key={index} onClick={() => togglePop(home)}>
+              <div className='card__image'>
+                <img src={home.image} alt="Home"/>
+              </div>
+              <div className='card__info'>
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> bds |
+                  <strong>{home.attributes[3].value}</strong> ba | 
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
             </div>
-            <div className='card__info'>
-              <h4>{home.attributes[0].value} ETH</h4>
-              <p>
-                <strong>{home.attributes[2].value}</strong> bds |
-                <strong>{home.attributes[3].value}</strong> ba | 
-                <strong>{home.attributes[4].value}</strong> sqft
-              </p>
-              <p>{home.address}</p>
-            </div>
+            ))}
           </div>
-          ))}
-        </div>
 
+        </div>
       </div>
+      )}
+
+      {toggle && (
+        <Home home={home} provider={provider} account={account} escrow={escrow} togglePop={togglePop}/>
+      )}
 
     </div>
   );
